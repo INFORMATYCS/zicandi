268469711c8e603ba31c4e99f590078519c963ed
Evert <evert.nicolas@gmail.com>
2020-03-18 21:30:23 -0600

    Agrega seguridad a la peticion http para index

--- app/Http/Controllers/ProveedorController.php
+++ app/Http/Controllers/ProveedorController.php
@@ -12,8 +12,10 @@ class ProveedorController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        if(!$request->ajax())return redirect('/');
+        
         $listaProveedores = Proveedor::all();
         return $listaProveedores;
     }
