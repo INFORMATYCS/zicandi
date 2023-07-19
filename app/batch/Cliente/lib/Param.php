0c6b4fb5ffee8a384b4e84cdc358198dc4043c6e
INFORMATYCS <evert.nicolas@gmail.com>
2023-07-19 13:51:16 -0600

    Migracion del proceso batch al servidor soccers-nas

--- app/batch/Cliente/lib/Param.php
+++ app/batch/Cliente/lib/Param.php
@@ -1,5 +1,6 @@
 <?php
 class Param{
     public static $_BASE_PATH_API = "http://localhost/";
-	public static $_BASE_PATH_PHP = "e:\\xampp\\php\\php";	
+	public static $_BASE_PATH_PHP = "e:\\xampp\\php\\php";
+    public static $_BASE_PATH_PHP_SYNOLOGY = "/opt/lampp/bin/php";	
 }    
